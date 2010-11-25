54cb56fccd14d2e67e31764c6e1a60954450809e
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2010-11-25 21:11:03 +0000

    - put all functions into a class LetoDMS_File

--- inc/inc.FileUtils.php
+++ inc/inc.FileUtils.php
@@ -18,158 +18,144 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-function renameFile($old, $new)
-{
-	return @rename($old, $new);
-}
-
-function removeFile($file)
-{
-	return @unlink($file);
-}
-
-function copyFile($source, $target)
-{
-	return @copy($source, $target);
-}
-
-function moveFile($source, $target)
-{
-	if (!@copyFile($source, $target))
-		return false;
-	return @removeFile($source);
-}
-
-function renameDir($old, $new)
-{
-	return @rename($old, $new);
-}
-
-function makeDir($path)
-{
-	/*
-	if (strncmp($path, DIRECTORY_SEPARATOR, 1) == 0) {
-		$mkfolder = DIRECTORY_SEPARATOR;
+class LetoDMS_File {
+	function renameFile($old, $new) {
+		return @rename($old, $new);
 	}
-	else {
-		$mkfolder = "";
+
+	function removeFile($file) {
+		return @unlink($file);
 	}
-	$path = preg_split( "/[\\\\\/]/" , $path );
-	for(  $i=0 ; isset( $path[$i] ) ; $i++ )
-	{
-		if(!strlen(trim($path[$i])))continue;
-		$mkfolder .= $path[$i];
-		
-		if( !is_dir( $mkfolder ) ){
-			$res=@mkdir( "$mkfolder" ,  0777);
-			if (!$res) return false;
+
+	function copyFile($source, $target) {
+		return @copy($source, $target);
+	}
+
+	function moveFile($source, $target) {
+		if (!@copyFile($source, $target))
+			return false;
+		return @removeFile($source);
+	}
+
+	function renameDir($old, $new) {
+		return @rename($old, $new);
+	}
+
+	function makeDir($path) {
+		/*
+		if (strncmp($path, DIRECTORY_SEPARATOR, 1) == 0) {
+			$mkfolder = DIRECTORY_SEPARATOR;
 		}
-		$mkfolder .= DIRECTORY_SEPARATOR;
-	}
-
-	return true;*/
-	
-	// patch from alekseynfor safe_mod or open_basedir 
-	
-	global $settings;
-	$path = substr_replace ($path, "/", 0, strlen($settings->_contentDir));
-
-	$mkfolder = $settings->_contentDir;
-    
-	$path = preg_split( "/[\\\\\/]/" , $path );
-        
-	for(  $i=0 ; isset( $path[$i] ) ; $i++ )
-	{
-		if(!strlen(trim($path[$i])))continue;
-		$mkfolder .= $path[$i];
-        
-		if( !is_dir( $mkfolder ) ){
-			$res= @mkdir( "$mkfolder" ,  0777);
-			if (!$res) return false;    
+		else {
+			$mkfolder = "";
 		}
-		$mkfolder .= DIRECTORY_SEPARATOR;
-	}
-    
-	return true;
-	
-}
-
-function removeDir($path)
-{
-	$handle = @opendir($path);
-	while ($entry = @readdir($handle) )
-	{
-		if ($entry == ".." || $entry == ".")
-			continue;
-		else if (is_dir($path . $entry))
+		$path = preg_split( "/[\\\\\/]/" , $path );
+		for(  $i=0 ; isset( $path[$i] ) ; $i++ )
 		{
-			if (!removeDir($path . $entry . "/"))
-				return false;
-		}
-		else
-		{
-			if (!@unlink($path . $entry))
-				return false;
-		}
-	}
-	@closedir($handle);
-	return @rmdir($path);
-}
+			if(!strlen(trim($path[$i])))continue;
+			$mkfolder .= $path[$i];
 
-function copyDir($sourcePath, $targetPath)
-{
-	if (mkdir($targetPath, 0777))
-	{
-		$handle = @opendir($sourcePath);
+			if( !is_dir( $mkfolder ) ){
+				$res=@mkdir( "$mkfolder" ,  0777);
+				if (!$res) return false;
+			}
+			$mkfolder .= DIRECTORY_SEPARATOR;
+		}
+
+		return true;*/
+
+		// patch from alekseynfor safe_mod or open_basedir
+
+		global $settings;
+		$path = substr_replace ($path, "/", 0, strlen($settings->_contentDir));
+
+		$mkfolder = $settings->_contentDir;
+
+		$path = preg_split( "/[\\\\\/]/" , $path );
+
+		for(  $i=0 ; isset( $path[$i] ) ; $i++ )
+		{
+			if(!strlen(trim($path[$i])))continue;
+			$mkfolder .= $path[$i];
+
+			if( !is_dir( $mkfolder ) ){
+				$res= @mkdir( "$mkfolder" ,  0777);
+				if (!$res) return false;
+			}
+			$mkfolder .= DIRECTORY_SEPARATOR;
+		}
+
+		return true;
+
+	}
+
+	function removeDir($path) {
+		$handle = @opendir($path);
 		while ($entry = @readdir($handle) )
 		{
 			if ($entry == ".." || $entry == ".")
 				continue;
-			else if (is_dir($sourcePath . $entry))
+			else if (is_dir($path . $entry))
 			{
-				if (!copyDir($sourcePath . $entry . "/", $targetPath . $entry . "/"))
+				if (!removeDir($path . $entry . "/"))
 					return false;
 			}
 			else
 			{
-				if (!@copy($sourcePath . $entry, $targetPath . $entry))
+				if (!@unlink($path . $entry))
 					return false;
 			}
 		}
 		@closedir($handle);
+		return @rmdir($path);
 	}
-	else
-		return false;
-	
-	return true;
-}
 
-function moveDir($sourcePath, $targetPath)
-{
-	if (!copyDir($sourcePath, $targetPath))
-		return false;
-	return removeDir($sourcePath);
-}
+	function copyDir($sourcePath, $targetPath) {
+		if (mkdir($targetPath, 0777)) {
+			$handle = @opendir($sourcePath);
+			while ($entry = @readdir($handle) ) {
+				if ($entry == ".." || $entry == ".")
+					continue;
+				else if (is_dir($sourcePath . $entry)) {
+					if (!copyDir($sourcePath . $entry . "/", $targetPath . $entry . "/"))
+						return false;
+				} else {
+					if (!@copy($sourcePath . $entry, $targetPath . $entry))
+						return false;
+				}
+			}
+			@closedir($handle);
+		}
+		else
+			return false;
 
-// code by Kioob (php.net manual)
-function gzcompressfile($source,$level=false)
-{
-	$dest=$source.'.gz';
-	$mode='wb'.$level;
-	$error=false;
-	if($fp_out=@gzopen($dest,$mode)){
-		if($fp_in=@fopen($source,'rb')){
-			while(!feof($fp_in))
-				@gzwrite($fp_out,fread($fp_in,1024*512));
-			@fclose($fp_in);
+		return true;
+	}
+
+	function moveDir($sourcePath, $targetPath) {
+		if (!copyDir($sourcePath, $targetPath))
+			return false;
+		return removeDir($sourcePath);
+	}
+
+	// code by Kioob (php.net manual)
+	function gzcompressfile($source,$level=false) {
+		$dest=$source.'.gz';
+		$mode='wb'.$level;
+		$error=false;
+		if($fp_out=@gzopen($dest,$mode)) {
+			if($fp_in=@fopen($source,'rb')) {
+				while(!feof($fp_in))
+					@gzwrite($fp_out,fread($fp_in,1024*512));
+				@fclose($fp_in);
+			}
+			else $error=true;
+			@gzclose($fp_out);
 		}
 		else $error=true;
-		@gzclose($fp_out);
-	}
-	else $error=true;
-	
-	if($error) return false;
-	else return $dest;
-} 
 
+		if($error) return false;
+		else return $dest;
+	}
+}
 ?>
