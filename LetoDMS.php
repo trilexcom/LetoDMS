4a8a7bdf1ee524a85fd71ca75a44190df4a23add
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2010-12-06 09:58:15 +0000

    - added files for pear package

---
+++ LetoDMS.php
@@ -0,0 +1,49 @@
+<?php
+//    MyDMS. Document Management System
+//    Copyright (C) 2010 Uwe Steinmann
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+/**
+ * @uses LetoDMS_DatabaseAccess
+ */
+require_once('inc/inc.DBAccess.php');
+
+/**
+ * @uses LetoDMS_DMS
+ */
+require_once('inc/inc.ClassDMS.php');
+
+/**
+ * @uses LetoDMS_Folder
+ */
+require_once('inc/inc.ClassFolder.php');
+
+/**
+ * @uses LetoDMS_Document
+ */
+require_once('inc/inc.ClassDocument.php');
+
+/**
+ * @uses LetoDMS_Group
+ */
+require_once('inc/inc.ClassGroup.php');
+
+/**
+ * @uses LetoDMS_User
+ */
+require_once('inc/inc.ClassUser.php');
+
+?>
