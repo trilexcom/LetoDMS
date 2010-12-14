23cf55f2025242db8ada4d0ca01120da97eb8d2a
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2010-12-14 12:16:39 +0000

    - added column 'date' to tblFolders

--- UPDATE-v3.0.0/update.sql
+++ UPDATE-v3.0.0/update.sql
@@ -1,4 +1,5 @@
-ALTER TABLE tblUsers ADD COLUMN role smallint(1) NOT NULL default '0' AFTER isAdmin;
-UPDATE tblUsers SET role = 1 WHERE isAdmin = 1;
-UPDATE tblUsers SET role = 2 WHERE id = 2;
+ALTER TABLE tblUsers ADD COLUMN `role` smallint(1) NOT NULL default '0' AFTER `isAdmin`;
+UPDATE tblUsers SET `role` = 1 WHERE `isAdmin` = 1;
+UPDATE tblUsers SET `role` = 2 WHERE `id` = 2;
 ALTER TABLE tblUsers DROP COLUMN isAdmin;
+ALTER TABLE tblFolders ADD COLUMN `date` int(12) default NULL AFTER `comment`;
