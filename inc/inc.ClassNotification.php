ba7b9d725c3d0bc8f5527b820f4d5155511c6be7
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-01-11 09:31:33 +0000

    - added new class for representing a notification

---
+++ inc/inc.ClassNotification.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Implementation of a notification object
+ *
+ * @category   DMS
+ * @package    LetoDMS
+ * @license    GPL 2
+ * @version    @version@
+ * @author     Uwe Steinmann <uwe@steinmann.cx>
+ * @copyright  Copyright (C) 2010 Uwe Steinmann
+ * @version    Release: @package_version@
+ */
+
+/**
+ * Class to represent a notification
+ *
+ * @category   DMS
+ * @package    LetoDMS
+ * @author     Uwe Steinmann <uwe@steinmann.cx>
+ * @copyright  Copyright (C) 2010 Uwe Steinmann
+ * @version    Release: @package_version@
+ */
+class LetoDMS_Notification { /* {{{ */
+	/**
+	 * @var integer id of target (document or folder)
+	 *
+	 * @access protected
+	 */
+	var $_target;
+
+	/**
+	 * @var integer document or folder
+	 *
+	 * @access protected
+	 */
+	var $_targettype;
+
+	/**
+	 * @var integer id of user to notify
+	 *
+	 * @access protected
+	 */
+	var $_userid;
+
+	/**
+	 * @var integer id of group to notify
+	 *
+	 * @access protected
+	 */
+	var $_groupid;
+
+	/**
+	 * @var object reference to the dms instance this user belongs to
+	 *
+	 * @access protected
+	 */
+	var $_dms;
+
+	function LetoDMS_Notification($target, $targettype, $userid, $groupid) {
+		$this->_target = $target;
+		$this->_targettype = $targettype;
+		$this->_userid = $userid;
+		$this->_groupid = $groupid;
+	}
+
+	function setDMS($dms) {
+		$this->_dms = $dms;
+	}
+
+	function getTarget() { return $this->_target; }
+
+	function getTargetType() { return $this->_targettype; }
+
+	function getUser() { return $this->_dms->getUser($this->_userid); }
+
+	function getGroup() { return $this->_dms->getGroup($this->_groupid); }
+} /* }}} */
+?>
