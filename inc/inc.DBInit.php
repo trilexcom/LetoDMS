6df309de9cfc975abf33f8cf22436d099936482b
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-01-31 11:05:15 +0000

    - use !empty() instead of isset() from _coreDir

--- inc/inc.DBInit.php
+++ inc/inc.DBInit.php
@@ -18,7 +18,7 @@
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-if(isset($settings->_coreDir))
+if(!empty($settings->_coreDir))
 	require_once($settings->_coreDir.'/LetoDMS_Core.php');
 else
 	require_once('LetoDMS/LetoDMS_Core.php');
