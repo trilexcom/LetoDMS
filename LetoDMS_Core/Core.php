df7a8b40f1558ed148ed477770dcc9ef688926a6
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-02-18 07:12:09 +0000

    - reorganisation of pear package

---
+++ LetoDMS_Core/Core.php
@@ -0,0 +1,74 @@
+<?php
+//    MyDMS. Document Management System
+//    Copyright (C) 2010 Uwe Steinmann
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+/**
+ * @uses LetoDMS_DatabaseAccess
+ */
+require_once('LetoDMS_Core/inc.DBAccess.php');
+
+/**
+ * @uses LetoDMS_DMS
+ */
+require_once('LetoDMS_Core/inc.ClassDMS.php');
+
+/**
+ * @uses LetoDMS_Folder
+ */
+require_once('LetoDMS_Core/inc.ClassFolder.php');
+
+/**
+ * @uses LetoDMS_Document
+ */
+require_once('LetoDMS_Core/inc.ClassDocument.php');
+
+/**
+ * @uses LetoDMS_Group
+ */
+require_once('LetoDMS_Core/inc.ClassGroup.php');
+
+/**
+ * @uses LetoDMS_User
+ */
+require_once('LetoDMS_Core/inc.ClassUser.php');
+
+/**
+ * @uses LetoDMS_KeywordCategory
+ */
+require_once('LetoDMS_Core/inc.ClassKeywords.php');
+
+/**
+ * @uses LetoDMS_Notification
+ */
+require_once('LetoDMS_Core/inc.ClassNotification.php');
+
+/**
+ * @uses LetoDMS_UserAccess
+ * @uses LetoDMS_GroupAccess
+ */
+require_once('LetoDMS_Core/inc.ClassAccess.php');
+
+/**
+ */
+require_once('LetoDMS_Core/inc.AccessUtils.php');
+
+/**
+ * @uses LetoDMS_File
+ */
+require_once('LetoDMS_Core/inc.FileUtils.php');
+
+?>
