a6222e84393b93f96a13db7f018a6261f6cc6835
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-03-10 14:50:07 +0000

    - added link to alternative upload method using jumploader

--- out/out.UpdateDocument.php
+++ out/out.UpdateDocument.php
@@ -107,6 +107,9 @@ $docAccess = $document->getApproversList();
 <tr>
 	<td class="warning"><?php echo getMLText("max_upload_size")." : ".ini_get( "upload_max_filesize"); ?></td>
 </tr>
+<tr>
+  <td><?php printf(getMLText('link_alt_updatedocument'), "out.UpdateDocument2.php?documentid=".$documentid); ?></td>
+</tr>
 </table><br>
 
 
