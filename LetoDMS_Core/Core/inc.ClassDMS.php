aaa9cbb0ec11444a9690e4507e4c46e3b81d85d0
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-04-05 06:39:44 +0000

    - fixed processing of creation date in search()

--- LetoDMS_Core/Core/inc.ClassDMS.php
+++ LetoDMS_Core/Core/inc.ClassDMS.php
@@ -462,17 +462,17 @@ class LetoDMS_Core_DMS {
 		// Is the search restricted to documents created between two specific dates?
 		$searchCreateDate = "";
 		if ($creationstartdate) {
-			$startdate = makeTimeStamp(0, 0, 0, $createstartdate["year"], $createstartdate["month"], $createstartdate["day"]);
+			$startdate = makeTimeStamp(0, 0, 0, $creationstartdate["year"], $creationstartdate["month"], $creationstartdate["day"]);
 			if ($startdate) {
 				$searchCreateDate .= "`tblDocuments`.`date` >= ".$startdate;
 			}
 		}
 		if ($creationenddate) {
-			$stopdate = makeTimeStamp(23, 59, 59, $createenddate["year"], $createenddate["month"], $createenddate["day"]);
+			$stopdate = makeTimeStamp(23, 59, 59, $creationenddate["year"], $creationenddate["month"], $creationenddate["day"]);
 			if ($stopdate) {
 				if($startdate)
 					$searchCreateDate .= " AND ";
-				$searchCreateDate = "`tblDocuments`.`date` <= ".$stopdate;
+				$searchCreateDate .= "`tblDocuments`.`date` <= ".$stopdate;
 			}
 		}
 
