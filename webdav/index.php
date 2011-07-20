43b968c533215590bc850f1650b2a6a5724498dd
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-07-20 07:19:06 +0000

    - updated on new configuration setting

--- webdav/index.php
+++ webdav/index.php
@@ -4,12 +4,13 @@ ini_set('include_path', '.:/etc/letodms-webdav:/usr/share/php');
 include("config.php");
 include("Log.php");
 include("letodms_webdav.php");
+include("../inc/inc.Settings.php");
 
-$db = new LetoDMS_Core_DatabaseAccess($g_config['type'], $g_config['hostname'], $g_config['user'], $g_config['passwd'], $g_config['name']);
-$db->connect() or die ("Could not connect to db-server \"" . $g_config['hostname'] . "\"");
+$db = new LetoDMS_Core_DatabaseAccess($settings->_dbDriver, $settings->_dbHostname, $settings->_dbUser, $settings->_dbPass, $settings->_dbDatabase);
+$db->connect() or die ("Could not connect to db-server \"" . $settings->_dbHostname . "\"");
 $db->_conn->Execute("set names 'utf8'");
 
-$dms = new LetoDMS_Core_DMS($db, $g_config['contentDir'].$g_config['contentOffsetDir']);
+$dms = new LetoDMS_Core_DMS($db, $settings->_contentDir.$settings->_contentOffsetDir);
 
 $log = Log::factory('file', $g_config['logfile']);
 
