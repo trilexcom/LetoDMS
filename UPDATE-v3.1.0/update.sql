5bbd1d0ff4683df3513e00941d1cbe2c5c468bc7
steinm <steinm@af50a49d-cfe1-4335-85d8-12766b9ec89a>
2011-07-27 06:24:46 +0000

    - do not make category name unique

--- UPDATE-v3.1.0/update.sql
+++ UPDATE-v3.1.0/update.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `tblCategory` (
   `id` int(11) NOT NULL auto_increment,
   `name` text NOT NULL,
-  PRIMARY KEY  (`id`),
-	UNIQUE (`name`)
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 INSERT INTO tblCategory VALUES (0, '');
 CREATE TABLE `tblDocumentCategory` (
